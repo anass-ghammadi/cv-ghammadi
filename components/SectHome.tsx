7355149c70862e8df33b33a981881db667ef2785
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 00:59:22 +0100

    Create SectHome.tsx

---
+++ components/SectHome.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import Ghammadi from '../assets/Anass-ghammadi.jpg';
+import Menu from './menu/Menu'
+import MenuMobile from './menu/Menu_mobile';
+
+
+
+
+
+function SectHome () {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+    return (
+        <>
+            <div className="">
+                
+                <Menu  toggle={toggle} />
+                <MenuMobile isOpen={isOpen} toggle={toggle} />
+                <div className=" flex flex-col items-center justify-center space-y-12 lg:flex-row lg:items-center lg:justify-center lg:space-x-36 lg:pt-16 lg:space-y-0">
+                    <div className="w-60 lg:w-96">
+                        <Image className="rounded-full outline outline-4 outline-blue-300 outline-offset-4 " src={Ghammadi} alt="anass ghammadi" />
+                    </div>
+                    <div className=" flex flex-col items-center justify-center space-y-5">
+                        <div>
+                            <h1 className="text-white font-bold text-2xl md:text-3xl ">Anass GHAMMADI</h1>
+                        </div>
+                        <div>
+                            <h1 className=" font-bold text-3xl md:text-5xl  text-blue-300">WEB DEVELOPPER</h1>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>      
+        </>
+    )
+    
+}
+
+export default  SectHome
