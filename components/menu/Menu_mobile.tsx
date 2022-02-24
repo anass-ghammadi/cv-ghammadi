3348458398079959d37457fa104086319ae780ec
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 00:59:20 +0100

    Create Menu_mobile.tsx

---
+++ components/menu/Menu_mobile.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+const MenuMobile: React.FC<{ toggle: () => void, isOpen: boolean }> = ({ toggle, isOpen }) => {
+    return (
+        <>
+            <header className="">
+                <nav className={isOpen ? "flex flex-col w-full items-center justify-between p-8 text-white " : "hidden"} onClick={toggle}>
+                    <div className="">
+                        <ul className=" flex flex-col space-y-2 text-lg font-semibold">
+                            <li className="">
+                            <Link href="/"><a>Home</a></Link>
+                            </li>
+
+                            <li className="">
+                            <Link href="/About"><a>About</a></Link>
+                            </li>
+
+                            <li className="">
+                            <Link href="/"><a>Portfolio</a></Link>
+                            </li>
+
+                            <li className="">
+                            <Link href="/"><a>Contact</a></Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>  
+            </header>
+        </>
+    )
+}
+
+export default MenuMobile;
