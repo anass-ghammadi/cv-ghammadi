4fc61e5cf79ad227d1893457cbe0d7a497fa3e30
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 01:49:56 +0100

    Create Waves.tsx

---
+++ components/Waves.tsx
@@ -0,0 +1,41 @@
+
+function Waves ()
+{
+    return (
+        <>
+            <div className="header">
+  
+  <div className="particles z-0">
+        
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+    </div>
+</div>
+        </>
+    )
+}
+
+export default Waves
