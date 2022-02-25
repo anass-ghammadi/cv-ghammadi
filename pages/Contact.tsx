35369264522602c1f5db879ab3d0de0ba8884a6b
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 01:55:11 +0100

    Update Contact.tsx

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,11 +1,11 @@
-
+import Waves from'../components/Waves';
 
 function Contact () {
     return (
         <>
             <div className="flex items-center justify-center pt-80 text-white text-3xl lg:text-6xl  font-semibold">
                 <h1>Comming Soon</h1>
-                
+                <div className="z-0"><Waves /></div> 
             </div>
         </>
     )
