29c045d28631e4f1b70f3e5ad90e7bb7f7a1920f
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 01:18:39 +0100

    Create About.tsx

---
+++ pages/About.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import SAbout from '../components/SectAbout'
+import Footer from '../components/Footer'
+
+function About ()
+{
+    return (
+        <>
+            <SAbout />
+            <Footer />
+        </>
+    )
+}
+
+export default About
