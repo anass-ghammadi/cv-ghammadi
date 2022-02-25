36ea2eb8eee16431e0a6a0e3623f55d40ab9f2d2
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 01:26:50 +0100

    Create Contact.tsx

---
+++ pages/Contact.tsx
@@ -0,0 +1,14 @@
+
+
+function Contact () {
+    return (
+        <>
+            <div className="flex items-center justify-center pt-80 text-white text-3xl lg:text-6xl  font-semibold">
+                <h1>Comming Soon</h1>
+                
+            </div>
+        </>
+    )
+}
+
+export default Contact
