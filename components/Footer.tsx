9b1693e48d40c5ec7f2d8724e43bf28927a2deb2
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 01:36:50 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,9 @@ function Footer () {
             <div className="flex items-center justify-center pt-5  space-x-2 lg:pt-14 lg:space-x-4">
                 <span className="border-t-4 px-5"></span>
                 <div className="flex w-36 space-x-2">
-                    <a href="#"><Image src={Linkedin} alt="Linkedin" /></a>
-                    <a href="#"><Image src={Github} alt="Github" /></a>
-                    <a href="#"><Image src={Facebook} alt="Facebook" /></a>
+                    <a href="https://www.linkedin.com/in/anass-ghammadi-a41711a4/"><Image src={Linkedin} alt="Linkedin" /></a>
+                    <a href="https://github.com/anass-ghammadi"><Image src={Github} alt="Github" /></a>
+                    <a href="https://www.facebook.com/anas.ghammadi/"><Image src={Facebook} alt="Facebook" /></a>
                 </div>
                 <span className="border-t-4 px-5"></span>
             </div>
