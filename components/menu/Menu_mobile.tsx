24c3a7a8bdabb5cfcfbeb8d7ee867058de49865d
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 02:34:58 +0100

    Update Menu_mobile.tsx

--- components/menu/Menu_mobile.tsx
+++ components/menu/Menu_mobile.tsx
@@ -4,8 +4,8 @@ const MenuMobile: React.FC<{ toggle: () => void, isOpen: boolean }> = ({ toggle,
     return (
         <>
             <header className="">
-                <nav className={isOpen ? "flex flex-col w-full items-center justify-between p-8 text-white " : "hidden"} onClick={toggle}>
-                    <div className="">
+                <nav className={isOpen ? " flex flex-col w-full items-center justify-between p-8 text-white " : "hidden"} onClick={toggle}>
+                    <div className="z-50">
                         <ul className=" flex flex-col space-y-2 text-lg font-semibold">
                             <li className="">
                             <Link href="/"><a>Home</a></Link>
