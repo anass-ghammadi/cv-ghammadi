18e13a971ccfc092bb48a9cf87684567f8d2a604
anass-ghammadi <anassghammadi@gmail.com>
2022-02-25 01:59:37 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import  '../styles/waves.css'
+import  '../styles/Waves.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
