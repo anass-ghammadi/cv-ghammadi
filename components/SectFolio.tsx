72429cbb8491ee582c94e5bad6fde969337740f9
anass-ghammadi <anassghammadi@gmail.com>
2022-03-03 23:44:14 +0100

    Update SectFolio.tsx

--- components/SectFolio.tsx
+++ components/SectFolio.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import SiteWeb from '../assets/website.png'
+import SiteWeb from '../assets/Website.png'
 import Agency from '../assets/agency.png'
 
 import View from '../assets/Group.png'
